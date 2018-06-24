9402561af62a87cdf59f90e32c7e1d2682527706
Earry Chen <earry.chen@foxmail.com>
2018-06-24 23:08:29 +0800

    add ajax csrftoken, add sweetalert

--- app01/views.py
+++ app01/views.py
@@ -139,3 +139,38 @@ def logout(request):
     rep = redirect("/login/")
     rep.delete_cookie("is_login")
     return rep
+
+
+def ajax_test(request):
+    return render(request, "app01/ajaxtest.html")
+
+def ajax_add(request):
+    print(request.POST)
+    i1 = request.POST.get("i1")
+    i2 = request.POST.get("i2")
+    i1 = int(i1)
+    i2 = int(i2)
+    print("i1:{} i2:{}".format(i1, i2))
+    ret = i1 + i2
+    return HttpResponse(ret)
+
+# django 序列化工具
+def persons(request):
+    ret = models.Person.objects.all()
+    # person_list = []
+    # for i in ret:
+    #     person_list.append({"name":i.name, "age":i.age})
+    # import json
+    # data = json.dumps(person_list)
+
+    # from django.core import serializers
+    # s = serializers.serialize("json", ret)
+    # print(s)
+    # return HttpResponse(s)
+    return render(request, "app01/sweetalert_demo.html", {"persons":ret})
+
+
+def delete(request):
+    del_id = request.POST.get("id")
+    models.Person.objects.filter(id=del_id).delete()
+    return HttpResponse("删除成功")
